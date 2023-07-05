b7e34c569f89dc68b4fd1c6c83b02232ce0d02ea
Prashant Rai <19ce073@gardividyapith.ac.in>
2023-07-06 01:56:11 +0530

    Update Crud API Done

--- connection/db.js
+++ connection/db.js
@@ -5,6 +5,11 @@ const url = 'mongodb://0.0.0.0:27017/crudtask';
 mongoose.connect(url).then(() => {
     console.log("Database Connected");
   })
-  .catch((error) => {
-    console.log("Error while connecting to the database:", error);
-  });
+  .catch((error) => console.log("Error while connecting to the database:", error));
+
+// try {
+//   mongoose.connect(url);
+//   console.log("Database Connected");
+// } catch (error) {
+//   console.log("Error while connecting to the database:", error);
+// }
--- routes/crud.js
+++ routes/crud.js
@@ -5,26 +5,27 @@ crudRoute.use(express.json());
 
 // ---------------------ADD USER API-----------------------//
 crudRoute.post('/crudadd', async(req, res) => {
-    // const {name, email, phone, password} = req.body
-    let userdata = {
-        name: req.body.name,
-        email: req.body.email,
-        phone: req.body.phone,
-        password: req.body.password
-        }
-
+    const {name, email, phone, password} = req.body
+    // let userdata = {
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     phone: req.body.phone,
+    //     password: req.body.password
+    //     }
     // console.log(userdata)
+    console.log(req.body)
     try {
-        const data = new crudModel(userdata)
+
+        const data = new crudModel(req.body)
         await data.save()
         console.log(data)
-        res.status(200).json({   success: true, message: "Data Added Successfully", data})
+        return res.status(200).json({   success: true, message: "Data Added Successfully", data})
     } catch (error) {
         console.log(error)
         if (error.keyPattern){
-            res.json({success: false, message: "User Exists Already"})
+            return res.json({success: false, message: "User Exists Already"})
         }
-        res.json({success: false, message: error})
+        return res.status(500).json({success: false, message: 'Internal Server Error maybe..', error: error.message })
     }
 })
 
@@ -33,10 +34,10 @@ crudRoute.post('/crudadd', async(req, res) => {
 crudRoute.get('/cruddata', async(req, res) => {
     try {
         const data = await crudModel.find()
-        res.status(200).json({success: true, message: "User Data Got Sucecssfully", data})
+        return res.status(200).json({success: true, message: "User Data Got Sucecssfully", data})
     } catch (error) {
         console.log(error)
-        res.status(500).json({success: false, message: error})
+        return res.status(500).json({success: false, message: error})
     }
 })
 
@@ -46,20 +47,38 @@ crudRoute.delete('/cruddata/:id', async(req, res) => {
     // console.log(id)
 
     try {
-        const deletedUser = await crudModel.findByIdAndDelete(id)
-        if (!deletedUser) {
+        const user = await crudModel.findByIdAndDelete(id)
+        if (!user) {
             return res.status(404).json({success: false, message: 'User not found' });
           }
         return res.json({ success: true, message: 'User Deleted Successfully' });
 
     } catch (error) {
         console.log(error)
-        res.status(500).json({success: false, message: error})
+        return res.status(500).json({success: false, message: error})
     }
 })
 
 
 // ---------------------UPDATE USER API-----------------------//
+crudRoute.put('/cruddata/:id', async(req, res) => {
+    const id = req.params.id
+    // console.log(id)
+    console.log(req.body)
+    try {
+        const data = await crudModel.findByIdAndUpdate(id, req.body, {new: true})
+        if (!data) {
+            return res.status(404).json({success: false, message: 'User not found' });
+            }
+        return res.json({ success: true, message: 'User Updated Successfully', data });
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({success: false, message: "An error occurred", error: error.message})
+    }
+})
+
+
+
 
 
 module.exports = crudRoute;
